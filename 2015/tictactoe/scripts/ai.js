a6c6e42c7c9d729f26d47efe0178ce4ef5d83819
Craig Sharkie <craig.sharkie@gmail.com>
2015-11-01 18:17:22 +1100

    Segregated the playerversusplayer JavaScript

---
+++ 2015/tictactoe/scripts/ai.js
@@ -0,0 +1,13 @@
+(function(){
+  var AI = {
+    MOVES: [
+      [true,true,false], [true,false,true], [false,true,true]
+    ],
+    init: function() {
+      // nothing is initiated here. The AI is purely responsive
+    }
+  }
+
+  window.AI = AI;
+  AI.init();
+}());
